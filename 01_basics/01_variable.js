5e85e18833a098316ee9db46e79ed35bc026b602
02SHALINI <164446225+02SHALINI@users.noreply.github.com>
2025-05-06 18:55:45 +0000

    Variable Declare done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,18 @@
+const accountId =143142
+let accountEmail = "senshalini79@gmail.com"
+var accountPassword= "12345"
+accountCity= "Satna"
+let accountState;
+//accountId = 2
+
+accountEmail="shalini123@gmail.com"
+accountPassword="425001"
+accountCity="Bengaluru Karnataka"
+
+console.log(accountId);
+
+console.table([accountId, accountPassword, accountEmail, accountCity, accountState])
+/*
+Prefer  not to use var
+because of issue in block  scope and functional scope
+*/
